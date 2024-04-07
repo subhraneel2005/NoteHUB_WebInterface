99a68eeba13043b9222eaa87ffa7dcb6d5de048a
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-04-07 14:49:41 +0530

    ui updated #2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import { HoverBorderGradientDemo } from "@/components/AccButton";
 import { NavbarDemo } from "@/components/Navbar";
+import { TabsDemo } from "@/components/Tabs";
 import { BackgroundBeams } from "@/components/ui/background-beams";
 import Link from "next/link";
 
 export default function Home(){
   return(
-    <div className="min-h-screen w-full bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+    <div className="min-h-screen w-full bg-neutral-950 relative flex flex-col items-center justify-center antialiased select-none">
       <NavbarDemo/>
     <div className="max-w-2xl mx-auto p-4">
       <h1 className="relative z-10 text-3xl md:text-5xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold mt-[120px]">
@@ -18,7 +20,9 @@ export default function Home(){
       Say goodbye to the fear of losing and managing physical notes. NoteHUB empowers students to effortlessly digitize and organize their notes, ensuring easy access and peace of mind. With NoteHUB, focus on learning, not on managing papers.Whether you're a student, professional or just someone who likes to jot down notes, we've got you covered.
       </p>
     </div>
+    <HoverBorderGradientDemo/>
     <BackgroundBeams />
+    <TabsDemo/>
   </div>
   )
 }
---
+++ src/components/Tabs.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import Image from "next/image";
+import { Tabs } from "./ui/tabs";
+
+export function TabsDemo() {
+  const tabs = [
+    {
+      title: "1st Step",
+      value: "product",
+      content: (
+        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
+          <p>Download the app</p>
+          <DummyContent />
+        </div>
+      ),
+    },
+    {
+      title: "2nd Step",
+      value: "services",
+      content: (
+        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
+          <p>Connect to your drive</p>
+          <DummyContent />
+        </div>
+      ),
+    },
+    {
+      title: "3rd Step",
+      value: "playground",
+      content: (
+        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
+          <p>Scan</p>
+          <DummyContent />
+        </div>
+      ),
+    },
+    {
+      title: "4th Step",
+      value: "content",
+      content: (
+        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
+          <p>Automatically uploaded to your drive</p>
+          <DummyContent />
+        </div>
+      ),
+    },
+    {
+      title: "5th Step",
+      value: "random",
+      content: (
+        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
+          <p>Now just chill</p>
+          <DummyContent />
+        </div>
+      ),
+    },
+  ];
+
+  return (
+    <div className="h-[20rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full  items-start justify-start my-40">
+      <Tabs tabs={tabs} />
+    </div>
+  );
+}
+
+const DummyContent = () => {
+  return (
+    <img
+      src="https://i.pinimg.com/564x/14/c9/ba/14c9ba29f5d27da98cdcd12edba318cb.jpg"
+      alt="dummy image"
+      width="1000"
+      height="1000"
+      className="object-cover object-left-top h-[60%]  md:h-[90%] absolute -bottom-10 inset-x-0 w-[90%] rounded-xl mx-auto"
+    />
+  );
+};
